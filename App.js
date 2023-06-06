249c70c742c3f60aebd6e609dbb9a672a2bc6fd9
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-06-06 15:38:30 +0300

    completed

---
+++ App.js
@@ -0,0 +1,44 @@
+import {
+  ImageBackground,
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import RegistrationScreen from "./screens/RegistrationScreen";
+import LoginScreen from "./screens/LoginScreen";
+import PostScreen from "./screens/PostsScreen";
+import { StatusBar } from "expo-status-bar";
+
+const image = require("./assets/BG.jpg");
+
+export default function App() {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <>
+        <ImageBackground source={image} style={styles.image} />
+
+        <View style={styles.container}>
+          <RegistrationScreen />
+          {/* <LoginScreen /> */}
+
+          {/* <PostScreen /> */}
+
+          <StatusBar style="dark" />
+        </View>
+      </>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+});
